7ec33b98f473d913c8a13506eba696062a550f00
Wade Phillips <phillips.acupuncture@gmail.com>
2019-01-30 11:56:10 -0900

    Build BreakoutFactory

--- database/factories/BreakoutFactory.php
+++ database/factories/BreakoutFactory.php
@@ -3,7 +3,12 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Breakout::class, function (Faker $faker) {
-    return [
-        //
-    ];
+  $title = $faker->sentence;
+  return [
+      'event_id' => $faker->numberBetween(0,50),
+      'title' => $title,
+      'slug' => str_slug($title, '-'),
+      'location' => $faker->words(rand(2,5), true ),
+      'description' => $faker->paragraphs(rand(1,3),true),
+  ];
 });
