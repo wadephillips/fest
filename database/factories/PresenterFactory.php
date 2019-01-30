490818a33372e1bd58ea6df29b700eab0254b4bc
Wade Phillips <phillips.acupuncture@gmail.com>
2019-01-30 11:27:45 -0900

    Add PresenterFactroy

--- database/factories/PresenterFactory.php
+++ database/factories/PresenterFactory.php
@@ -4,6 +4,8 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Presenter::class, function (Faker $faker) {
     return [
-        //
+        'name' => $faker->name,
+        'credentials' => $faker->optional(.8)->words(3, true),
+        'bio' => $faker->paragraph(3,true)
     ];
 });
