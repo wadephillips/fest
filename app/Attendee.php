4fe4879cb67cd684fbab97f20f086da0d435eae2
Wade Phillips <phillips.acupuncture@gmail.com>
2019-01-30 11:47:47 -0900

    create Attendee factory

--- app/Attendee.php
+++ app/Attendee.php
@@ -7,6 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Attendee extends Model
 {
 
+  /**
+   * The attributes that should be cast to native types.
+   *
+   * @var array
+   */
+  protected $casts = [
+      'modifiers' => 'array',
+  ];
+
   public function event()
   {
     return $this->hasOne('App\Event');
--- database/factories/AttendeeFactory.php
+++ database/factories/AttendeeFactory.php
@@ -3,7 +3,29 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Attendee::class, function (Faker $faker) {
-    return [
-        //
-    ];
+  $json = json_encode([
+      [ 'option' => $faker->word, 'price' => $faker->numberBetween(0, 20000), 'quantity' => $faker->numberBetween(0,10),],
+      [ 'option' => $faker->word, 'price' => $faker->numberBetween(0, 20000), 'quantity' => $faker->numberBetween(0,10),],
+      [ 'option' => $faker->word, 'price' => $faker->numberBetween(0, 20000), 'quantity' => $faker->numberBetween(0,10),],
+  ]);
+
+  return [
+      'event_id' => $faker->numberBetween(1,10),
+      'payment_id' => $faker->numberBetween(1,50),
+      'name' => $faker->name,
+      'email' => $faker->email,
+      'phone' => $faker->phoneNumber,
+      'address' => $faker->streetAddress,
+      'address_2' => $faker->optional()->secondaryAddress,
+      'suite' => $faker->optional()->secondaryAddress,
+      'city' => $faker->city,
+      'state' => $faker->stateAbbr,
+      'postal' => $faker->postcode,
+      'country' => $faker->countryCode,
+      'emergency_contact_name' => $faker->name,
+      'emergency_contact_phone' => $faker->phoneNumber,
+      'emergency_contact_relation' => $faker->word,
+      'modifiers' => $json,
+      'total' => $faker->numberBetween(0,250000)
+  ];
 });
