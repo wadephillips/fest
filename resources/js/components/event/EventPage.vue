672d4ff7bcebc378c96b5b89327b21122e256099
Wade Phillips <phillips.acupuncture@gmail.com>
2019-02-28 11:53:21 -0900

    working on navbars

--- resources/js/components/event/EventPage.vue
+++ resources/js/components/event/EventPage.vue
@@ -1,169 +1,181 @@
 <template>
   <main>
-    <!--Hero-->
-    <info-section>
-      <template slot="heading">
-        <h1 class="text-center">{{event.name}}</h1>
-      </template>
+    <nav-menu :linkObjects="this.links" ></nav-menu>
+    <div data-offset="0" data-spy="scroll" data-target="#eventNavbar">
+      <!--Hero-->
+      <info-section>
+        <template slot="heading">
+          <h1 class="text-center">{{event.name}}</h1>
+        </template>
 
-      <template slot="subheading">
-        <h3 class="text-center">{{this.eventDates}}</h3>
-      </template>
+        <template slot="subheading">
+          <h3 class="text-center">{{this.eventDates}}</h3>
+        </template>
 
-      <template slot="default">
-        <div class="col-md">
-          <p class="lead">{{ event.description}}</p>
-        </div>
-      </template>
-
-    </info-section>
-
-    <hr>
-    <info-section>
-      <template slot="heading">
-        <h2 class="text-center">Theme</h2>
-      </template>
-      <template slot="subheading">
-        <h3 class="text-center">Practical Interventions</h3>
-      </template>
-      <template slot="image-left">
-        <div class="col-md-6">
-          <img src="/img/chair.svg" class="img-responsive" alt="" />
-        </div>
-      </template>
-      <template slot="default">
-        <div class="col-md-6 text-center pt-4"><p class="lead">At this POCAfest we are gonna look at the basics: what makes a CA clinic
-          work and not just work-sing.</p>
-          <p>We are gonna do this in two ways:</p>
-          <p>1) Lisa Rohleder is making the keynote speech on The Hierarchy of Needs that all CA clinics need to take
-            care
-            of in order to be a sustainable small business. Many of the Breakouts relate to this hierarchy (and you can
-            read about it in Prick, Prod, and Provoke https://www.pocacoop.com/prick-prod-provoke/)</p>
-          <p>2) We will also be hosting our first auricular training for punks and non punks led by Ellen Vincent,
-            Caroline Picker, Susan Williams, Amy Severinsen and Olivia Crane. (For a full description of this, read
-            below,
-            right after the logistics of registration and traveling to POCAfest and above the Schedule.)</p></div>
-      </template>
-    </info-section>
-
-    <hr>
-    <info-section>
-      <template slot="heading">
-        <h2 class="text-center">POCA Auricular Acu-Technician Training and Certification</h2>
-      </template>
-      <template slot="subheading">
-        <p class="lead text-center">Join us for the inaugural POCA Auricular Acu-Technician Training and Certification
-        Course!</p>
-      </template>
-      <template slot="default">
-        <div class="col-md-6 mt-3">
-          <p>Learn the 5 Needle Protocol for addiction, trauma, and behavioral health. Anyone is welcome to take this training, though <strong>space is limited</strong>.</p>
-
-          <p>For non-acupuncturists, attending this entire series of breakout sessions is the first step towards earning
-            an Auricular Acu-Technican Certificate.** </p>
-
-          <p>Included in the registration fee is the second step towards certification: access to our 14-hour online
-            course materials about trauma-informed auricular acupuncture, starting with the history of auricular
-            acupuncture, the 5 Needle Protocol and its applications / implementation, the neuroscience of trauma and
-            addiction as well as the social and structural determinants of health & addiction, harm reduction, the
-            neuroscience of acupuncture, and issues surrounding legislation. The third and final step towards
-            certification will be the successful completion of 40 ear treatments, either supervised at an in-person ear
-            clinic or if necessary, via distance-supervision. </p>
-          <p><strong>Attending this course in its entirety is strongly encouraged for any licensed   acupuncturists who are interested in becoming a POCA-certified Trainer of Auricular Acu-Technicians.</strong> We will get into detail (and answer all of your questions!) about the certification process itself, as well as how and why we got here, in one of the breakout sessions.</p>
-
-          <p class="small">**Attending this training does not guarantee that the trainee will be able to legally
-            practice auricular acupuncture or the 5NP in trainee's state of residence. If a Wisconsin resident, this
-            Auricular Acu-Technician Certificate will enable trainee to practice this protocol legally under physician
-            delegation.</p>
-
-        </div>
-      </template>
-      <template slot="image-right">
-        <div class="col-md-6">
-          <img src="/img/ear.svg" class="img-responsive" alt="" />
-        </div>
-      </template>
-    </info-section>
-    <hr>
-    <!--Location-->
-    <info-section>
-      <template slot="heading">
-        <h2 class="text-center">Location</h2>
-
-      </template>
-
-      <template slot="default">
-        <div class="col-md-6">
-          <location-address
-              :name="this.event.location_name"
-              :address="this.event.address"
-              :address_2="this.event.address_2"
-              :suite="this.event.suite"
-              :city="this.event.city"
-              :state="this.event.state"
-              :postal="this.event.postal"
-          ></location-address>
-        </div>
-      </template>
-      <template slot="image-right">
-        <div class="col-md">
-        <fest-map place-id="ChIJFY5ircKXBYgRXS2rAIsCc4Q" api-key="AIzaSyCSdbPjbn7KYWaFGl6Cy8EYD1l1YVCTGL8"></fest-map>
-        </div>
-      </template>
-    </info-section>
-
-    <hr>
-    <!--Fees-->
-    <info-section>
-
-      <template slot="heading">
-        <h2 class="text-center">Conference Fees</h2>
-      </template>
-
-      <template slot="default">
-        <hr>
-        <div class="col-md">
-          <div class="text-center" v-for="fee in this.event.fees">
-            <fee :fee="fee"></fee>
+        <template slot="default">
+          <div class="col-md">
+            <p class="lead">{{ event.description}}</p>
           </div>
+        </template>
+
+      </info-section>
+
+      <hr>
+      <info-section id="theme">
+        <template slot="heading">
+          <h2 class="text-center">Theme</h2>
+        </template>
+        <template slot="subheading">
+          <h3 class="text-center">Practical Interventions</h3>
+        </template>
+        <template slot="image-left">
+          <div class="col-md-6">
+            <img alt="" class="img-responsive" src="/img/chair.svg"/>
+          </div>
+        </template>
+        <template slot="default">
+          <div class="col-md-6 text-center pt-4"><p class="lead">At this POCAfest we are gonna look at the basics: what
+            makes a CA clinic
+            work and not just work-sing.</p>
+            <p>We are gonna do this in two ways:</p>
+            <p>1) Lisa Rohleder is making the keynote speech on The Hierarchy of Needs that all CA clinics need to take
+              care
+              of in order to be a sustainable small business. Many of the Breakouts relate to this hierarchy (and you
+              can
+              read about it in Prick, Prod, and Provoke https://www.pocacoop.com/prick-prod-provoke/)</p>
+            <p>2) We will also be hosting our first auricular training for punks and non punks led by Ellen Vincent,
+              Caroline Picker, Susan Williams, Amy Severinsen and Olivia Crane. (For a full description of this, read
+              below,
+              right after the logistics of registration and traveling to POCAfest and above the Schedule.)</p></div>
+        </template>
+      </info-section>
+
+      <hr>
+      <info-section id="auricular">
+        <template slot="heading">
+          <h2 class="text-center">POCA Auricular Acu-Technician Training and Certification</h2>
+        </template>
+        <template slot="subheading">
+          <p class="lead text-center">Join us for the inaugural POCA Auricular Acu-Technician Training and Certification
+            Course!</p>
+        </template>
+        <template slot="default">
+          <div class="col-md-6 mt-3">
+            <p>Learn the 5 Needle Protocol for addiction, trauma, and behavioral health. Anyone is welcome to take this
+              training, though <strong>space is limited</strong>.</p>
+
+            <p>For non-acupuncturists, attending this entire series of breakout sessions is the first step towards
+              earning
+              an Auricular Acu-Technican Certificate.** </p>
+
+            <p>Included in the registration fee is the second step towards certification: access to our 14-hour online
+              course materials about trauma-informed auricular acupuncture, starting with the history of auricular
+              acupuncture, the 5 Needle Protocol and its applications / implementation, the neuroscience of trauma and
+              addiction as well as the social and structural determinants of health & addiction, harm reduction, the
+              neuroscience of acupuncture, and issues surrounding legislation. The third and final step towards
+              certification will be the successful completion of 40 ear treatments, either supervised at an in-person
+              ear
+              clinic or if necessary, via distance-supervision. </p>
+            <p><strong>Attending this course in its entirety is strongly encouraged for any licensed acupuncturists who
+              are interested in becoming a POCA-certified Trainer of Auricular Acu-Technicians.</strong> We will get
+              into detail (and answer all of your questions!) about the certification process itself, as well as how and
+              why we got here, in one of the breakout sessions.</p>
+
+            <p class="small">**Attending this training does not guarantee that the trainee will be able to legally
+              practice auricular acupuncture or the 5NP in trainee's state of residence. If a Wisconsin resident, this
+              Auricular Acu-Technician Certificate will enable trainee to practice this protocol legally under physician
+              delegation.</p>
+
+          </div>
+        </template>
+        <template slot="image-right">
+          <div class="col-md-6">
+            <img alt="" class="img-responsive" src="/img/ear.svg"/>
+          </div>
+        </template>
+      </info-section>
+      <hr>
+      <!--Location-->
+      <info-section id="location">
+        <template slot="heading">
+          <h2 class="text-center">Location</h2>
+
+        </template>
+
+        <template slot="default">
+          <div class="col-md-6">
+            <location-address
+                :address="this.event.address"
+                :address_2="this.event.address_2"
+                :city="this.event.city"
+                :name="this.event.location_name"
+                :postal="this.event.postal"
+                :state="this.event.state"
+                :suite="this.event.suite"
+            ></location-address>
+          </div>
+        </template>
+        <template slot="image-right">
+          <div class="col-md">
+            <fest-map api-key="AIzaSyCSdbPjbn7KYWaFGl6Cy8EYD1l1YVCTGL8"
+                      place-id="ChIJFY5ircKXBYgRXS2rAIsCc4Q"></fest-map>
+          </div>
+        </template>
+      </info-section>
+
+      <hr>
+      <!--Fees-->
+      <info-section id="fees">
+
+        <template slot="heading">
+          <h2 class="text-center">Conference Fees</h2>
+        </template>
+
+        <template slot="default">
+          <hr>
+          <div class="col-md">
+            <div class="text-center" v-for="fee in this.event.fees">
+              <fee :fee="fee"></fee>
+            </div>
+          </div>
+        </template>
+
+      </info-section>
+
+      <hr>
+      <!--schedule-->
+      <info-section id="schedule">
+        <template slot="heading">
+          <h2 class="text-center">Conference Schedule</h2>
+        </template>
+
+        <schedule></schedule>
+      </info-section>
+
+      <hr>
+      <!--Breakouts-->
+      <info-section id="breakouts">
+        <template slot="heading">
+          <h2 class="text-center">Breakouts</h2>
+        </template>
+
+        <div class="card-columns">
+          <breakout :breakout="breakout" :key="breakout.id" ey v-for="breakout in this.event.breakouts"></breakout>
         </div>
-      </template>
 
-    </info-section>
+      </info-section>
 
-    <hr>
-    <!--schedule-->
-    <info-section>
-      <template slot="heading">
-        <h2 class="text-center">Conference Schedule</h2>
-      </template>
-
-      <schedule></schedule>
-    </info-section>
-
-    <hr>
-    <!--Breakouts-->
-    <info-section>
-      <template slot="heading">
-        <h2 class="text-center">Breakouts</h2>
-      </template>
-
-      <div class="card-columns">
-        <breakout :breakout="breakout" :key="breakout.id" ey v-for="breakout in this.event.breakouts"></breakout>
-      </div>
-
-    </info-section>
-
-    <hr>
-    <!--Presenters-->
-    <info-section>
-      <template slot="heading">
-        <h2 class="text-center">Presenters</h2>
-      </template>
-      <div class="card-columns">
-        <presenter :presenter="presenter" :key="presenter.id" v-for="presenter in this.presenters"></presenter>
-      </div>
-    </info-section>
+      <hr>
+      <!--Presenters-->
+      <info-section id="presenters">
+        <template slot="heading">
+          <h2 class="text-center">Presenters</h2>
+        </template>
+        <div class="card-columns">
+          <presenter :key="presenter.id" :presenter="presenter" v-for="presenter in this.presenters"></presenter>
+        </div>
+      </info-section>
+    </div>
   </main>
 </template>
 
@@ -181,6 +193,77 @@
     props: ['event', 'presenters'],
     components: {FestMap, LocationAddress, Breakout, Presenter, InfoSection, Fee},
     mixins: [dates],
+    data() {
+      return {
+        links: [
+          {
+            a: "/events/" + this.event.slug + "/register",
+            description: 'Register',
+            styleClasses: 'btn btn-success'
+          },
+          {
+            a: '#theme',
+            description: 'Theme',
+            styleClasses: 'nav-link'
+          },
+          {
+            a: '#auricular',
+            description: 'Auricular Certification',
+            styleClasses: 'nav-link'
+          },
+          {
+            a: '#location',
+            description: 'Location',
+            styleClasses: 'nav-link'
+          },
+          {
+            a: '#fees',
+            description: 'Fees',
+            styleClasses: 'nav-link'
+          },
+          {
+            a: '#schedule',
+            description: 'Schedule',
+            styleClasses: 'nav-link'
+          },
+          {
+            a: '#breakouts',
+            description: 'Breakouts',
+            styleClasses: 'nav-link'
+          },
+          {
+            a: '#presenters',
+            description: 'Presenters',
+            styleClasses: 'nav-link'
+          },
+        ]
+      }
+    },
+    computed: {
+      registrationUrl() {
+        return "/events/" + this.event.slug + "/register"
+      }
+    },
+    created() {
+      // The function actually applying the offset
+      function offsetAnchor() {
+        if (location.hash.length !== 0) {
+          window.scrollTo(window.scrollX, window.scrollY - 100);
+        }
+      }
+
+      // Captures click events of all <a> elements with href starting with #
+      $(document).on('click', 'a[href^="#"]', function (event) {
+        // Click events are captured before hashchanges. Timeout
+        // causes offsetAnchor to be called after the page jump.
+        window.setTimeout(function () {
+          offsetAnchor();
+        }, 0);
+      });
+
+      // Set the offset when entering page with hash present in the url
+      window.setTimeout(offsetAnchor, 0);
+    }
   }
 </script>
 
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -14,7 +14,7 @@
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-  <a class="navbar-brand" href="#">Navbar</a>
+  <a class="navbar-brand" href="/"><img src="/img/chair.svg" alt="POCA Recliner Logo"></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01"
           aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -23,22 +23,15 @@
   <div class="collapse navbar-collapse" id="navbarColor01">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Features</a>
+        <a class="nav-link" href="/events">Events</a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">About</a>
-      </li>
+      {{--<li class="nav-item">--}}
+        {{--<a class="nav-link" href="#">Pricing</a>--}}
+      {{--</li>--}}
     </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="text" placeholder="Search">
-      <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
-    </form>
   </div>
 </nav>
 <div id="app" v-cloak>
@@ -47,7 +40,7 @@
   </main>
 </div>
 <footer>
-  <div class="container">
+  <div class="container-fluid bg-secondary pt-3">
     <div class="row m-3">
       <div class="col-md-4">
         <address>
