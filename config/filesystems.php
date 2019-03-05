288c045ad9e3a6c632f9d6bafeb55684a90bf4d8
Wade Phillips <phillips.acupuncture@gmail.com>
2019-03-05 10:57:43 -0900

    extract vendor.js from app.js and update public disk url

--- config/filesystems.php
+++ config/filesystems.php
@@ -51,7 +51,8 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => '/storage',
+//            'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
         ],
 
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -65,6 +65,8 @@
   </div>
 </footer>
 
+<script src="{{ mix('js/manifest.js') }}"></script>
+<script src="{{ mix('js/vendor.js') }}"></script>
 <script src="{{ mix('js/app.js') }}"></script>
 </body>
 </html>
