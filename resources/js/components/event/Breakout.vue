5aacb022e3b0aa9a9234b4ac70a16bfcad2623e9
Wade Phillips <phillips.acupuncture@gmail.com>
2019-03-06 06:43:48 -0900

    formatting the breakout blocks

--- resources/js/components/event/Breakout.vue
+++ resources/js/components/event/Breakout.vue
@@ -4,18 +4,18 @@
       {{ this.breakout.title }}
     </div>
     <div class="card-body">
-      <p class="card-title">
-        {{this.breakout.start}}
-        <span v-if="this.breakout.end !== '' || this.breakout.end !== null"> to {{ this.breakout.end}}</span>
-        <span> on {{this.breakout.date}}</span>
+      <p class="card-title" v-if="this.breakout.start">
+        {{this.formatedTime(this.breakout.start)}}
+        <span v-if="this.breakout.end !== '' || this.breakout.end !== null"> to {{ this.formatedTime(this.breakout.end)}}</span>
+        <span> on {{this.formatedDate(this.breakout.date)}}</span>
 
       </p>
-      <p class="card-title"><span> At {{ this.breakout.location }}</span></p>
+      <p class="card-title" v-if="this.breakout.location"><em> At {{ this.breakout.location }}</em></p>
       <p class="card-text">{{ this.breakout.description }}</p>
     </div>
     <div class="card-footer text-muted">
       <span><strong>Presenters:</strong></span>
-      <span v-for="(presenter, key, i) in this.breakout.presenters">{{presenter.name}}<span v-if="i != Object.keys(breakout.presenters).length - 1">, </span></span>
+      <span v-for="(presenter, key, i) in this.breakout.presenters">{{presenter.name}}<span v-if="key !== Object.keys(breakout.presenters).length - 1">, </span></span>
     </div>
   </div>
 </template>
@@ -24,7 +24,22 @@
   import moment from 'moment';
   export default {
     name: "Breakout",
-    props: ['breakout']
+    props: ['breakout'],
+    data() {
+      return {
+        dateFormat: "ddd, MMM Do",
+        timeFormat: "h:mm A"
+      }
+    },
+    methods: {
+      formatedDate(date) {
+        return moment(date).format(this.dateFormat);
+      },
+      formatedTime(time) {
+        let dateTime = this.breakout.date + " " + time
+        return moment(dateTime).format(this.timeFormat);
+      }
+    }
   }
 </script>
 
