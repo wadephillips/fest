e062fdcb7fc94a67500fefee2231ab3b2cf3e6b5
Wade Phillips <phillips.acupuncture@gmail.com>
2021-09-06 06:31:42 -0800

    Comments for EventsController

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -3,96 +3,45 @@
 namespace App\Http\Controllers;
 
 use App\Event;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 use function compact;
-use function dd;
-use Illuminate\Http\Request;
 use function redirect;
-use function response;
 use function secure_url;
 use function view;
 
 class EventController extends Controller
 {
+
     /**
-     * Display a listing of the resource.
+     * Handle requests to the index page.
      *
-     * @return \Illuminate\Http\Response
+     * @return View|RedirectResponse|
      */
     public function index()
     {
         $events = Event::all()->where('active', '=', 1);
-        if ($events->count() == 1) {
+
+        if ($events->count() == 1) { // if there is only one event redirect to its detail page
             return redirect(secure_url('/events/'.$events[0]->slug));
-        } else {
+        } else { // otherwise show a list of active events
             return view('events', compact('events'));
         }
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-    }
 
     /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
+     * Display the specified event detail.
      *
      * @param  \App\Event  $event
-     * @return \Illuminate\Http\Response
+     * @return View
      */
     public function show(Event $event)
     {
         $event->load(['breakouts.presenters', 'fees']);
+
         $presenters = $event->presenters;
 
         return view('event.index', compact('event', 'presenters'));
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Event  $event
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Event $event)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Event  $event
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Event $event)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Event  $event
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Event $event)
-    {
-        //
     }
 }
