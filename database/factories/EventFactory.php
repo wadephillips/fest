b16f9d64eb3a15014d06e10d9f83176d33c331eb
Wade Phillips <phillips.acupuncture@gmail.com>
2021-09-06 06:31:10 -0800

    Update EventFactory for use with new faker library

--- database/factories/EventFactory.php
+++ database/factories/EventFactory.php
@@ -1,6 +1,7 @@
 <?php
 
 use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 $factory->define(App\Event::class, function (Faker $faker) {
     $name = $faker->sentence;
@@ -9,18 +10,18 @@ $factory->define(App\Event::class, function (Faker $faker) {
 
     return [
       'name' => $name,
-      'slug' => str_slug($name, '-'),
+      'slug' => Str::slug($name, '-'),
       'start' => $date,
       'end' => $end,
       'description' => $faker->paragraph(3, true),
       'active' => 1,
-      'location_name' => $faker->company,
-      'address' => $faker->streetAddress,
+      'location_name' => $faker->company(),
+      'address' => $faker->streetAddress(),
       'address_2' => $faker->optional()->secondaryAddress,
       'suite' => $faker->optional()->secondaryAddress,
-      'city' => $faker->city,
-      'state' => $faker->stateAbbr,
-      'postal' => $faker->postcode,
-      'country' => $faker->countryCode,
+      'city' => $faker->city(),
+      'state' => $faker->stateAbbr(),
+      'postal' => $faker->postcode(),
+      'country' => $faker->countryCode(),
   ];
 });
